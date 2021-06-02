859b02208c52d5d9861880779417e99ebd5090b6
luisrdevy <luisrdevy480@gmail.com>
2021-06-02 09:50:43 -0500

    Home title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
+import { Grid, Typography } from "@material-ui/core";
+
 const Home = () => {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h2">Papermin</Typography>
+          <Typography variant="h4">
+            La mejor forma de administrar tu papeleria
+          </Typography>
+        </Grid>
+      </Grid>
+      {/* <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h3">Caracteristicas</Typography>
+          <Typography variant="h4">
+            La mejor forma de administrar tu papeleria
+          </Typography>
+        </Grid>
+      </Grid> */}
+    </>
   );
 };
 
